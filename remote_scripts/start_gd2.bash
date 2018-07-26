79f0070a54c95244b843321c8abf3511ea989522
rohantmp <rohantmp@gmail.com>
2018-07-26 18:51:21 +0530

    Update start_gd2.bash

--- remote_scripts/start_gd2.bash
+++ remote_scripts/start_gd2.bash
@@ -2,6 +2,6 @@ export DIR=$(dirname "$0")
 source ${DIR}/exports.bash
 
 set -u
-tmux new-session -d -s gd2 &&
-  tmux send-keys -t gd2 "rm -f /usr/local/var/run/glusterd2/glusterd2.socket" C-m
+tmux new-session -d -s gd2
+tmux send-keys -t gd2 "rm -f /usr/local/var/run/glusterd2/glusterd2.socket" C-m
 tmux send-keys -t gd2 "glusterd2 --config ${GD2_CONF}" C-m
