f16da9a2ef2a23faef6111487ab2fba51f67a7ae
Rohan CJ <rojoseph@redhat.com>
2018-07-26 16:44:29 +0530

    Defin DIR in exports too.

--- remote_scripts/exports.bash
+++ remote_scripts/exports.bash
@@ -2,6 +2,8 @@
 # export DIR=$(dirname "$0")
 # source ${DIR}/exports.bash
 
+export DIR=$(dirname "$0")
+
 export GD2_CONF="${DIR}/glusterd2.toml"
 export ETCD_CONF="${DIR}/etcd_config"
 export GOLANG="https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz"
