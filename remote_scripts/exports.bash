bb0c9bf27fc71cbc13833d14804aec2ee6a6af8d
Rohan CJ <rojoseph@redhat.com>
2018-07-26 15:33:37 +0530

    etcd.yml works

--- remote_scripts/exports.bash
+++ remote_scripts/exports.bash
@@ -1,3 +1,4 @@
-export GD2_CONF="glusterd2.toml"
-export ETCD_CONF="etcd_config"
+export DIR=$(dirname "$0")
+export GD2_CONF="${DIR}/glusterd2.toml"
+export ETCD_CONF="${DIR}/etcd_config"
 export GOLANG="https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz"
