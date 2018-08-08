e96f0940fca3d294f525d1fff9ecd06460f431ec
Rohan CJ <rojoseph@redhat.com>
2018-08-08 20:54:15 +0530

    Add ssh options to ignore hostkey

--- setup_gd2.bash
+++ setup_gd2.bash
@@ -2,6 +2,7 @@
 set -eu
 DIR=$(dirname "$0")
 testdir="/root/test"
+ssh_opts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 source ${DIR}/exports.bash
 if [[ $# -eq 1 ]]
 then
@@ -9,15 +10,15 @@ then
 elif [[ $# -eq 2 ]]
 then
         export HOST=$1
-        sci-pass $2 ${HOST}
+        sci-pass $2 ${HOST} ${ssh_opts}
 else
   echo "no host provided"
   exit 1
 fi
 
-ssh ${HOST} "mkdir -p ${testdir}"
-ssh ${HOST} "yum install -y tmux"
+ssh ${HOST} ${ssh_opts} "mkdir -p ${testdir}"
+ssh ${HOST} ${ssh_opts} "yum install -y tmux"
 scp ${DIR}/files/.bashrc ${HOST}:/root/
 scp ${DIR}/remote_scripts/*.bash ${HOST}:${testdir}/
-ssh ${HOST} "tmux new-session -d -s setup; tmux send-keys -t setup 'bash --verbose ${testdir}/setup_gd2_node_master.bash' C-m"
-ssh ${HOST} -t 'tmux a -t setup'
+ssh ${HOST} ${ssh_opts} "tmux new-session -d -s setup; tmux send-keys -t setup 'bash --verbose ${testdir}/setup_gd2_node_master.bash' C-m"
+ssh ${HOST} ${ssh_opts} -t 'tmux a -t setup'
