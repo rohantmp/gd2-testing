c174c279a608ab9a37047787cc53741b025ba910
Rohan CJ <rojoseph@redhat.com>
2018-08-26 22:16:29 +0530

    Add local setup

--- setup_node.bash
+++ setup_node.bash
@@ -6,7 +6,14 @@ ssh_opts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 source ${DIR}/exports.bash
 testdir=${HOME}/test
 
+echo [running as ${USER}]
+echo [creating working directory at ${testdir}]
+
 mkdir -p ${testdir}
 rsync ${DIR}/files/.bashrc ${HOME}
 rsync ${DIR}/remote_scripts/*.bash ${testdir}/
 bash --verbose ${testdir}/setup_gd2_node_master.bash
+
+
+echo [running as ${USER}]
+echo [creating working directory at ${testdir}]
